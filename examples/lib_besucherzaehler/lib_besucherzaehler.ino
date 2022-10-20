ba92b7b57b16ee546a918f620945b497d7d2a87f
Rolf Rahm <rahm@fes-es.de>
2022-10-20 21:55:26 +0200

    Update lib_besucherzaehler.ino

--- examples/lib_besucherzaehler/lib_besucherzaehler.ino
+++ examples/lib_besucherzaehler/lib_besucherzaehler.ino
@@ -1,6 +1,6 @@
 #include <Wire.h>                      // Wire Bibliothek für I2C-Display
 #include <LiquidCrystal_I2C.h>
-#include "pinToggle.h"
+#include <esp32CarrierBoard.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);    // I2C-Display: adresse 0x27, 2 Zeilen á 16 Zeichen
                         
