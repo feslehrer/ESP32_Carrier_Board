cd2f841a8090a667e7a33625626272273c65eb17
Rolf Rahm <rahm@fes-es.de>
2025-01-16 18:36:36 +0100

    Update 8_1_demo_pwm.ino

--- examples/LFB/8_1_demo_pwm/8_1_demo_pwm.ino
+++ examples/LFB/8_1_demo_pwm/8_1_demo_pwm.ino
@@ -8,12 +8,19 @@ void setup()
   ledcAttach(SPEAKER,1000,12);           //GPIO 27
 }
 
+int play = false;
+
 void loop()
-{ 
-  // ADC hat 12-Bit Auflösung
+{ // ADC hat 12-Bit Auflösung
   int hell = analogRead(A3);   // GPIO 34
   ledcWrite(LED_BUILDIN, hell);
   //(GPIO, duty_cycle)
   ledcWrite(BACKLIGHT,4096-hell);
+  if (!play)
+  { ledcWriteNote(SPEAKER,NOTE_A,4); // GPIO,Note,Oktave
+    delay(3000);
+    ledcWriteTone(SPEAKER,0);        // GPIO, Frequenz
+    play = true;   
+  }
   delay(50);
 }
