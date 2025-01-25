9cfccc885570e3fb86c20de3055a9d16cb6fe323
Rolf Rahm <rahm@fes-es.de>
2025-01-25 12:43:10 +0100

    Update 3_4_lib_analog_mapping.ino

--- examples/LFB/3_4_lib_analog_mapping/3_4_lib_analog_mapping.ino
+++ examples/LFB/3_4_lib_analog_mapping/3_4_lib_analog_mapping.ino
@@ -13,19 +13,22 @@ void setup()
 
 char buf[17];
 
+// Spannungsauflösung des ADC
+const float Ulsb = 3.3/pow(2,12);
+
 void loop()
 {
   uint16_t wert = analogRead(A3);   //GPIO 34
   Serial.print(wert);
   Serial.print(" -> ");
 
-  float spannung = 0.0008057 * (float)wert;
+  float spannung = Ulsb * wert;
   spannung = fMap(spannung,0.0,3.3,0.0,5.0);
   Serial.print(spannung); Serial.println("V");
   sprintf(buf,"U = %4.2fV",spannung);
   lcd.setCursor(0,1);lcd.print(buf);
 
-  delay(50);
+  delay(200);
 }
 
 float adcMap(float x, float x_min, float x_max, float y_min, float y_max)
