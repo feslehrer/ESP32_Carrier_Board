3cee8dd2fbf7a5b8dcbc0db89dfb077159d63887
Rolf Rahm <rahm@fes-es.de>
2025-02-22 08:46:24 +0100

    Update lib_Temperaturmessung_LM75.ino

--- examples/lib_Temperaturmessung_LM75/lib_Temperaturmessung_LM75.ino
+++ examples/lib_Temperaturmessung_LM75/lib_Temperaturmessung_LM75.ino
@@ -6,7 +6,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
-  lm75_init();
+  lm75_init(lm75Adresse);
   Wire.begin();
   Serial.begin(9600);
   Serial.println("Temperatur");
@@ -17,7 +17,7 @@ void setup()
 
 void loop()
 {
-  float temp = lm75_read();
+  float temp = lm75_read(lm75Adresse);
   Serial.println(temp);
   lcd.setCursor(0,1);
   lcd.print(temp);lcd.print("\337C"); 
